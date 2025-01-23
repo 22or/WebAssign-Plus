294b59e62dc1dedc52276f96b16446212d4461a8
Yarnacle <109094587+Yarnacle@users.noreply.github.com>
2025-01-22 20:46:21 -0600

    reorganize code, make injection earlier and with context, add instant scrolling and auto focusing

--- src/script.js
+++ src/script.js
@@ -1,48 +1,136 @@
-// press enter to submit
-let questionDivs = document.querySelectorAll('div[wa-qa]');
-let success = true;
-for (let questionDiv of questionDivs) {
-	let button;
-	try {
-		button = questionDiv.getElementsByClassName('qUtility')[0]
-			.getElementsByClassName('qButtons')[0]
-			.getElementsByClassName('submitSaveStandard')[0]
-			.getElementsByClassName('primarySolidUnifiedButton')[0];
+const htmlElem = document.getElementsByTagName('html')[0];
+
+// DOM dependent stuff
+window.onload = () => {
+	focusNextQuestion();
+	setUpEnterKeySubmit();
+	setUpAnswerPreviews();
+};
+
+// instant scroll
+htmlElem.style.scrollBehavior = 'auto';
+HTMLElement.prototype.scrollIntoView = () => {
+	console.debug('Scroll blocked');
+};
+
+// focus setup
+const originalFocus = HTMLElement.prototype.focus;
+HTMLElement.prototype.focus = () => {
+	console.debug('Focus blocked');
+};
+function focus(elem) {
+	HTMLElement.prototype.focus = originalFocus;
+	elem.focus();
+	if (elem.tagName == 'INPUT' && elem.type == 'text') {
+		const val = elem.value;
+		elem.value = '';
+		elem.value = val;
 	}
-	catch {
-		success = false;
-		break;
-	}
-	let submitScript =  button.getAttribute('onclick');
-	
-	let fieldSpans = questionDiv.getElementsByClassName('qTextField')
-	
-	for (let fieldSpan of fieldSpans) {
-		inputField = fieldSpan.getElementsByTagName('input')[0];
-		inputField.setAttribute('onkeypress','if (event.keyCode == 13) {' + JSON.stringify(submitScript.substring(0,submitScript.indexOf('return false'))).slice(1,-1) + '}');
-	}
-}
-if (success ) {
-	console.log("Input fields updated");
-}
-else {
-	console.log("Failed to update input fields")
+	HTMLElement.prototype.focus = () => {};
 }
 
-// preview answers
-function displayPreview(inputField) {
-	let box = inputField;
-    let questionId = inputField.getAttribute('name');
-    let container = document.getElementById(`${questionId}_container`);
-    if (!box.value) {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-        return;
-    }
-    if (container.innerHTML == '') {
-        container.innerHTML = 'Loading';
-    }
+function focusNextQuestion() {
+	const url = window.location.href;
+	const i = url.indexOf('#');
+	if (i == -1) {
+		return;
+	}
+	const id = url.substring(i + 1);
+	const questionDiv = document.querySelector(`div[wa-qa="${id}_container"]`);
+	const inputFields = questionDiv.querySelectorAll('input[type="text"]');
+	let focused = false;
+	for (const inputField of inputFields) {
+		const mark = document.getElementById(`${inputField.name}_mark`);
+		if (!mark || mark.classList.contains('mIncorrect')) {
+			focus(inputField);
+			focused = true;
+			break;
+		}
+	}
+	if (!focused) { // question is complete; move on to next
+		let questionNumber = parseInt(questionDiv.getAttribute('data-view-position'));
+		const newQuestionDiv = document.querySelector(`div[data-view-position="${questionNumber + 1}"]`);
+		focus(newQuestionDiv.querySelector('input[type="text"]'));
+	}
+	else {
+		console.log('Question focused');
+	}
+}
+
+function setUpEnterKeySubmit() {
+	const questionDivs = document.querySelectorAll('div[wa-qa]');
+	let success = true;
+	for (const questionDiv of questionDivs) {
+		let button;
+		try {
+			button = questionDiv.getElementsByClassName('qUtility')[0]
+				.getElementsByClassName('qButtons')[0]
+				.getElementsByClassName('submitSaveStandard')[0]
+				.getElementsByClassName('primarySolidUnifiedButton')[0];
+		}
+		catch {
+			success = false;
+			break;
+		}
+		const submitScript =  button.getAttribute('onclick');
+		
+		const fieldSpans = questionDiv.getElementsByClassName('qTextField');
+		for (const fieldSpan of fieldSpans) {
+			inputField = fieldSpan.getElementsByTagName('input')[0];
+			inputField.setAttribute('onkeypress','if (event.keyCode == 13) {' + JSON.stringify(submitScript.substring(0,submitScript.indexOf('return false'))).slice(1,-1) + '}');
+		}
+	}
+	if (success ) {
+		console.log("Input fields updated");
+	}
+	else {
+		console.log("Failed to update input fields; questions not submittable?")
+	}
+}
+
+function setUpAnswerPreviews() {
+	const previewButtons = document.querySelectorAll('a.smPreview');
+	for (const previewButton of previewButtons) {
+		const inputField = previewButton.previousElementSibling.getElementsByTagName('input')[0];
+		if (!inputField) {
+			continue;
+		}
+		if (!inputField.value) {
+			continue;
+		}
+		const questionId = inputField.getAttribute('name');
+		
+		// add container
+		const previewContainer = document.createElement('div');
+		previewContainer.setAttribute('id',`${questionId}_container`);
+		document.querySelector(`input[value="${questionId.toUpperCase()}"]`)
+			.insertAdjacentElement('beforebegin',previewContainer);
+		const br = document.createElement('br');
+		const br2 = document.createElement('br');
+		previewContainer.insertAdjacentElement('beforebegin',br);
+		previewContainer.insertAdjacentElement('beforebegin',br2);
+
+		// bind live update event
+		inputField.onkeyup = () => {
+			displayPreview(inputField);
+		};
+		displayPreview(inputField);
+	}
+}
+
+function displayPreview (inputField) {
+	const box = inputField;
+	const questionId = inputField.getAttribute('name');
+	const container = document.getElementById(`${questionId}_container`);
+	if (!box.value) {
+		while (container.firstChild) {
+			container.removeChild(container.firstChild);
+		}
+		return;
+	}
+	if (container.innerHTML == '') {
+		container.innerHTML = 'Loading';
+	}
 
 	// webassign's parsing
 	var boxVal = escape(box.value);
@@ -55,39 +143,12 @@ function displayPreview(inputField) {
 	}
 
 	// display preview
-	let img = document.createElement('img');
-    img.onload = () => {
-        container.appendChild(img);
-        while (container.firstChild != img) {
-            container.removeChild(container.firstChild);
-        }
-    };
-    img.src = `/cgi-perl/symimage.cgi?size=4&expr=${boxVal}`;
-}
-
-let url = "https://www.webassign.net/v4cgi/extra/symbolicPreviewer.html"
-let previewButtons = document.querySelectorAll('a.smPreview');
-for (let previewButton of previewButtons) {
-	let inputField = previewButton.previousElementSibling.getElementsByTagName('input')[0];
-	if (!inputField) {
-		continue;
-	}
-	if (!inputField.value) {
-		continue;
-	}
-    let questionId = inputField.getAttribute('name');
-	
-	// add container
-    let previewContainer = document.createElement('div');
-    previewContainer.setAttribute('id',`${questionId}_container`);
-    document.querySelector(`input[value="${questionId.toUpperCase()}"]`)
-		.insertAdjacentElement('beforebegin',previewContainer);
-    let br = document.createElement('br');
-    let br2 = document.createElement('br');
-    previewContainer.insertAdjacentElement('beforebegin',br);
-    previewContainer.insertAdjacentElement('beforebegin',br2);
-
-	// bind live update event
-	inputField.setAttribute('onkeyup','displayPreview(this)');
-	displayPreview(inputField);
+	const img = document.createElement('img');
+	img.onload = () => {
+		container.appendChild(img);
+		while (container.firstChild != img) {
+			container.removeChild(container.firstChild);
+		}
+	};
+	img.src = `/cgi-perl/symimage.cgi?size=4&expr=${boxVal}`;
 }
